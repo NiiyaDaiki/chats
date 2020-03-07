3c7a92e75d448c44dcccd614fc90d3098d41a8e5
Niiya Daiki <daiki.niiya@fox-hound.tech>
2020-03-08 05:00:00 +0900

    conf:HMR設定

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080
+    }
+});
