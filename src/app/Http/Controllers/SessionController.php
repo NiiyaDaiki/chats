fe43908f12205e67c86bc65217a6052878e9dc0f
Niiya Daiki <daiki.niiya@fox-hound.tech>
2020-03-13 03:02:06 +0900

    private_session作成

--- src/app/Http/Controllers/SessionController.php
+++ src/app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SessionEvent;
 use App\Http\Resources\SessionResource;
 use App\Models\Session;
 use Illuminate\Http\Request;
@@ -11,7 +12,8 @@ class SessionController extends Controller
     public function create(Request $request)
     {
         $session = Session::create(['user1_id' => auth()->id(), 'user2_id' => $request->friend_id]);
-
-        return new SessionResource($session);
+        $modifiedSession = new SessionResource($session);
+        broadcast(new SessionEvent($modifiedSession, auth()->id()));
+        return $modifiedSession;
     }
 }
